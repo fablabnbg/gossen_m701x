6572dc963098e1a03e2eb05578cea035f61a9de0
Jürgen Weigert <juewei@fabfolk.com>
2014-09-21 00:52:00 +0200

    pull

--- gossen_metrawatt_parse.py
+++ gossen_metrawatt_parse.py
@@ -1,38 +1,48 @@
 #! /usr/bin/python
-
-import re,sys,string
+#
+# (C) 2014 jw@fabfolk.com
+# Distribute under MIT License or ask.
+ 
+import re,sys,string,serial
 
 str = "\x13DATIMx=20.09.14;18:33$18\r\n\x11"
 str = re.sub("[\x13\x11\r\n]+", "", str)
-(cmd,csum) = string.split(str, '$')
-if (csum != checksum(cmd)):
-  print "Checksum error '%s': %s != %s" % (str,csum,checksum(cmd))
-  sys.exit(1)
+# (cmd,csum) = string.split(str, '$')
+# if (csum != checksum(cmd)):
+#   print "Checksum error '%s': %s != %s" % (str,csum,checksum(cmd))
+#   sys.exit(1)
 
 
 class M701x:
+  """
+  Tolle Klasse
+  """
   def __init__(self, serial_port):
     self.port = serial_port
     self.serial = serial.Serial(port=self.port, baudrate=9600, )
 
   def read(self):
+    """reads until X_ON seen, (one low level protocol frame)"""
     self.serial.readline(eol='\x11')
 
   def write(self,str):
+    """adds X_OFF, $, checksum, CR, X_ON to str, 
+       and sends it to the serial line
+    """
     self.serial.write('\x13' + str + '$' + checksum(str) + '\r\x11')
 
-  def checksum(str):
+  def checksum(self,str):
     qsum_dec = ord('$')
     for i in str:
       d = ord(i)
       qsum_dec += d
     return "%x" % (qsum_dec & 0xff)
 
-
-m701 = M701x('/dev/ttyUSB0')
-m701.write("Hallo")
-str= m701.read()
-print str
+if __name__ == "__main__":
+  m701 = M701x('/dev/ttyUSB0')
+  m701.write("Hallo")
+  str= m701.read()
+  print str
 
 
 
