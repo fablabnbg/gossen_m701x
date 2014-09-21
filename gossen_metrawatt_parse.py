3966e59601f8a7dc839af2b59742339e7c85fcda
Mose <mose@fabfolk.com>
2014-09-21 15:13:47 +0200

    Tidied up so code actually works. Tested with SI+ Module.

--- gossen_metrawatt_parse.py
+++ gossen_metrawatt_parse.py
@@ -1,32 +1,41 @@
 #! /usr/bin/python
 #
-# (C) 2014 jw@fabfolk.com, All rights reserved.
+# (C) 2014 juewei@fabfolk.com, All rights reserved.
 #
 # Distribute under MIT License or ask.
-# 
- 
+#
+# Authors: mose@fabfolk.com, juewei@fabfolk.com
+
+
 import re,sys,string,serial
 
 
 class M701x:
   """
-  Tolle Klasse
+  Class for interfacing with Gossen Metrawatt devices over serial
   """
   def __init__(self, serial_port):
     self.port = serial_port
-    self.serial = serial.Serial(port=self.port, baudrate=9600, )
+    self.serial = serial.Serial(
+                    port=self.port,
+                    baudrate=9600,
+                    bytesize=serial.EIGHTBITS,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE,
+                    timeout=3,
+                    xonxoff=True
+                  )
 
   def read(self):
-    """reads until X_ON seen, (one low level protocol frame)"""
-    self.serial.readline(eol='\x11')
+    """reads one line"""
+    return self.serial.readline()
 
   def write(self,str):
-    """adds X_OFF, $, checksum, CR, X_ON to str, 
-       and sends it to the serial line
-    """
-    self.serial.write('\x13' + str + '$' + checksum(str) + '\r\x11')
+    """adds $-delimiter, checksum and line ending to str and sends it to the serial line"""
+    self.serial.write(str + '$' + self.checksum(str) + '\r\n')
 
   def checksum(self,str):
+    """calculates checksum of a request/answer"""
     qsum_dec = ord('$')
     for i in str:
       d = ord(i)
@@ -34,9 +43,9 @@ class M701x:
     return "%x" % (qsum_dec & 0xff)
 
 if __name__ == "__main__":
-  m701 = M701x('/dev/ttyUSB0')
-  m701.write("Hallo")
-  str= m701.read()
+  m701 = M701x(sys.argv[1])
+  m701.write("IDN?")
+  str = m701.read()
   print str
 
   # str = "\x13DATIMx=20.09.14;18:33$18\r\n\x11"
